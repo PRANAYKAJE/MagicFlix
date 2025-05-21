d9207576b9220f43e27eef357f77df943cbcf147
PRANAYKAJE <pranaykaje9765@gmail.com>
2025-05-21 20:57:36 +0530

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,39 @@
+// Footer Component
+import "./styles/Footer.css";
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="footer">
+                <div id="row1" className="row ">
+                    <a
+                        href="https://www.linkedin.com/in/pranay-kaje-b27b46273/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <i className="fa fa-linkedin"></i>
+                    </a>
+                    <a
+                        href="https://github.com/PRANAYKAJE/MagicFlix"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <i className="fa-brands fa-github"></i>
+                    </a>
+                    <a
+                        href="https://www.instagram.com/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <i className="fa fa-instagram"></i>
+                    </a>
+                </div>
+                <div id="row1" className="row">
+                    © Developed <i class="fa-solid fa-heart"></i> By Pranay Kaje
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
